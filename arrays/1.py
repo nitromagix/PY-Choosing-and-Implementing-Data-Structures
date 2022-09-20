8da507d0861df424783d2803cbf74f1070ed1947
Martin Prost <martinprost@flatbox.local>
2022-09-19 20:32:04 -0700

    finished assignment

--- arrays/1.py
+++ arrays/1.py
@@ -1,4 +1,27 @@
 import numpy as np
-a = np.array([1,2,3,4])
+a = np.array([1, 2, 3, 4])
 
-print(a[0])
+print(a)
+
+def print_one_per_line(arr):
+    for item in arr:
+        print(item)
+
+print_one_per_line(a)
+
+
+def sum_of_array(arr):
+    sum = 0
+    for item in arr:
+        sum += item
+    return sum
+
+print(sum_of_array(a))
+
+def mulitply_by_three(arr):
+    return np.array(list(map(lambda x: x * x, arr)))
+
+m = mulitply_by_three(a)
+print(m)
+
+print_one_per_line(m)
